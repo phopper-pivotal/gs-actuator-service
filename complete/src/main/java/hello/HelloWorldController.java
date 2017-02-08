ba4fc6e0bdebabf98caef2a146a6960cd21232c5
phopper-pivotal <phopper@pivotal.io>
2017-02-08 07:03:40 -0600

    add manifest and expose a REST controller to dump vcap_application data as JSON on root context

--- complete/src/main/java/hello/HelloWorldController.java
+++ complete/src/main/java/hello/HelloWorldController.java
@@ -1,23 +1,27 @@
 package hello;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @Controller
-@RequestMapping("/hello-world")
+//@RequestMapping("/hello-world")
 public class HelloWorldController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping(method=RequestMethod.GET)
+    @RequestMapping(value = "hello-world", method=RequestMethod.GET)
     public @ResponseBody Greeting sayHello(@RequestParam(value="name", required=false, defaultValue="Stranger") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody String getVcap() {
+        return System.getenv("VCAP_APPLICATION");
+    }
 }
